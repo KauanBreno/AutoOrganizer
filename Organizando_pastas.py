bc3fdaf02fb239876609d64ab61784502e118872
Kauan <kauan.com18kn@gmail.com>
2025-05-04 19:24:49 -0300

    criei um novo laço de repetição que move todos arquivos para suas respectivas pastas separadas por tipo

--- Organizando_pastas.py
+++ Organizando_pastas.py
@@ -4,8 +4,7 @@ from pathlib import Path
 
 def organize_files():
     
-
-    types = ['exe', 'txt', 'jpg', 'png', 'pdf', 'docx', 'pdf', 'zip']
+    types = ['exe', 'txt', 'jpg', 'png', 'pdf', 'docx', 'zip']
 
     raiz = Path.home() / "Downloads"
     
@@ -14,6 +13,17 @@ def organize_files():
         new_folder = raiz / type
         new_folder.mkdir(exist_ok=True)
 
-    for file in raiz.iterdir():
-        name_file = file.name
+    for type in types:
+        for file in raiz.glob(f"*.{type}"):
+            file_name = file.name
+            new_path_folder = raiz / type / file_name
+            file.rename(new_path_folder)
+            
+    
+    print("Pasta Downloads organizada com sucesso!")        
+if __name__ == "__main__":
+    organize_files()
+            
+        
+            
         
